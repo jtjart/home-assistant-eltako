ca206585251a3c17e666752248f6e33dba10761e
jtjart <80978647+jtjart@users.noreply.github.com>
2025-07-05 10:12:22 +0200

    Added await

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -145,7 +145,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     set_gateway_to_hass(hass, usb_gateway)
 
     hass.data[DATA_ELTAKO][DATA_ENTITIES] = {}
-    hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
 
