2614032c9568f3f21cdc0e06619665e14ca43fdb
jtjart <80978647+jtjart@users.noreply.github.com>
2025-07-05 09:56:54 +0200

    fix wrong function name

--- custom_components/eltako/eltako_integration_init.py
+++ custom_components/eltako/eltako_integration_init.py
@@ -145,7 +145,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     set_gateway_to_hass(hass, usb_gateway)
 
     hass.data[DATA_ELTAKO][DATA_ENTITIES] = {}
-    hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
+    hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
 
